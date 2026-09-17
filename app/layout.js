71beaa91b8209610c2f34391f91eeaf756ff8303
Cody Chih <cody@slicelife.com>
2026-09-16 20:05:25 -0400

    Load V2 editorial styles

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
 import './globals.css'
+import './v2.css'
 export const metadata={title:'The Brief of Ordinary Gentleman',description:'Sports, culture and investigations from the League of Ordinary Gentlemen'}
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
