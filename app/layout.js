435aa2c9b7a6a5b29a041ea5c85455ee5ef0b960
Cody Chih <cody@slicelife.com>
2026-09-16 21:35:36 -0400

    Load transaction feed styling

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import './v2.css'
 import './polish.css'
+import './wire.css'
 export const metadata={title:'The Brief of Ordinary Gentleman',description:'Sports, culture and investigations from the League of Ordinary Gentlemen'}
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
