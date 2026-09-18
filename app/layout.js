5120c417739440ee95aa2a53ec96dedc0543f05d
Cody Chih <cody@slicelife.com>
2026-09-18 15:02:27 -0400

    Load culture pagination styles

--- app/layout.js
+++ app/layout.js
@@ -4,5 +4,6 @@ import './polish.css'
 import './wire.css'
 import './editorial-pass.css'
 import './home-pass.css'
+import './culture-pagination.css'
 export const metadata={title:'The Brief of Ordinary Gentleman',description:'Sports, culture and investigations from the League of Ordinary Gentlemen'}
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
