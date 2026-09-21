d3bfc9e186f3e9a693629516f2bc674858d67f89
Cody Chih <cody@slicelife.com>
2026-09-20 22:24:20 -0400

    Publish Sabine public statement

--- app/articles/writers.js
+++ app/articles/writers.js
@@ -62,6 +62,7 @@ export const writers={
 }
 
 export const articleWriters={
+  'sabine-chest-speculation-statement':'march',
   'hollis-body-keeps-score':'crane',
   'maude-jayden-daniels-elbow':'gannon',
   'sabine-all-ugly-lloyd':'march',
